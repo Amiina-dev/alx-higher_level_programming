cb0efc2f21208c58ea3e26c194521b56908952bb
Amab <miynaoje@outlook.com>
2022-11-30 10:18:02 -0800

    made exec

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,7 +2,7 @@
 for i in range(ord('z'), ord('a')-1, -1):
     print('{:c}'.format(i) if i % 2 == 0 else chr(i-32), end='')
 
-#or
+# or
 # for i in range(0, 26):
 #    c = ord('z') - i
 #    if i % 2 == 1:
