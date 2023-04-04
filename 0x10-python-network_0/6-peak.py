c9ac541ac70bb6772edd74ed2f8a9fbb5dbd14a8
Amab <miynaoje@outlook.com>
2023-04-04 04:49:55 -0700

    Task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,19 @@
+#!/bin/bash
+"""
+Contains the function find_peak to find the peak
+"""
+
+def find_peak(list_of_integers):
+    """
+    finds a peak in a list of unsorted integers
+    """
+    list = list_of_integers
+    l = len(list)
+    if l == 0:
+        return
+    m = l // 2
+    if (m == l - 1 or list[m] >= list[m + 1]) and (m == 0 or list[m] >= list[m - 1]):
+        return list[m]
+    if m != l - 1 and list[m + 1] > list[m]:
+        return find_peak(list[m + 1:])
+     return find_peak(list[:m])
